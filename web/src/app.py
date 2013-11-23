fec58837c2c36643a05f61165b6c4193fe0e3137
Jonathan Villemaire-Krajden <odontomachus@gmail.com>
2013-11-23 16:55:39 -0500

    Added templates and functionality.

--- web/src/app.py
+++ web/src/app.py
@@ -1,37 +1,52 @@
+import os
 import configparser
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 import tornado.web
+import tornado.template
 
-from models import *  # import the engine to bind
+import models
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r"/([a-zA-Z0-9]{32})", HomeHandler),
+            (r"/(?P<user>[a-zA-Z0-9]{32})", HomeHandler),
         ]
         cp = configparser.ConfigParser()
         cp.read('site.cfg');
         (db_name, db_user, db_pass) = map(lambda x: cp.get('db', x), ('db', 'user', 'pass'))
 
-        settings = dict(
-            cookie_secret=cp.get('app', 'cookie_secret'),
-            debug=cp.get('app', 'debug').strip().lower() == "true"
-        )
+        settings = {
+            'cookie_secret':cp.get('app', 'cookie_secret'),
+            'template_path':os.path.join(os.path.dirname(__file__), "templates"),
+            'debug':cp.get('app', 'debug').strip().lower() == "true",
+            'xsrf_cookies':True,
+        }
         tornado.web.Application.__init__(self, handlers, **settings)
         # Have one global connection.
-        self.db = scoped_session(sessionmaker(bind=engine))
+        self.db = scoped_session(sessionmaker(bind=models.engine))
 
 class DBHandler(tornado.web.RequestHandler):
     @property
     def db(self):
         return self.application.db
 
-class HomeHandler(DBHandler):
-    def get(self, hash):
-        user = self.db.query(User).filter_by(hash=hash).one()
-        self.write(user.name)
-    
+class AuthHandler(DBHandler):
+    def decode_argument(self, value, name=None):
+        if name=='user':
+            user = self.db.query(models.User).filter_by(hash=value).first()
+            if not user:
+                raise tornado.web.HTTPError(403, 
+                    "Veuillez utiliser le lien avec votre jeton d'authentification.")
+            else:
+                return user
+        else:
+            return value
+
+class HomeHandler(AuthHandler):
+    def get(self, user):
+        self.render("template.html", user=user)
+
 
 class ViewHandler(DBHandler):
     def get(self, hash):
@@ -40,5 +55,5 @@ class ViewHandler(DBHandler):
 application = Application()
 
 if __name__ == "__main__":
-    application.listen(8888)
+    application.listen(8119)
     tornado.ioloop.IOLoop.instance().start()
