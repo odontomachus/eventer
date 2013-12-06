117fb8f5634e08fca2ba88810ac4f45b37bda3fd
Jonathan Villemaire-Krajden <odontomachus@gmail.com>
2013-12-06 11:47:58 -0500

    Refactoring as I get the hang of angular.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,4 +1,54 @@
-var app = angular.module("club", ["ui.keypress", "ngSanitize"]);
+var app = angular.module("club", ["ui.keypress", "ngSanitize"]).
+    constant('days', {'V': 0, 'S':1, 'D':2, 'L':3}).
+    value('callbacks', {}).
+    run(function (callbacks) {
+        // Setup etherpad
+        // var padId = $('#wikipad').attr('padId');
+        // $('#wikipad').pad({'padId':padId,
+        //                    'showChat':'false',
+        //                    'host':window.location.protocol + "//" + window.location.hostname+":9001",
+        //                    'userName': eventer.name,
+        //                    });
+
+        // Setup back-end updates channel
+        var getWs = function(timeout) {
+            var innerWs = new WebSocket("ws://"+window.location.hostname+eventer.home+"/updates");
+            innerWs.onmessage = function(evt) {
+	        var data = $.parseJSON(evt.data);
+	        for(var dataKey in data) {
+                    var callback = callbacks[dataKey]
+                    var obj = data[dataKey];
+                    if (callback) {
+                        callback(obj);
+                    };
+	        };
+            };
+            innerWs.onclose = function (evt) {
+                var nextTimeout = Math.min(timeout*1.2 + 5, 3000);
+                setTimeout(function () {
+                    ws = getWs(nextTimeout);
+                }, timeout*1000);
+            }
+            /**
+               Set connection retry timeout to a low value upon a successful connection.
+            */
+            innerWs.onopen = function (evt) {
+                setTimeout(function () {
+                    if (innerWs.readyState == WebSocket.OPEN) {
+                        timeout = 1;
+                        innerWs.onclose = function (evt) {
+                            var nextTimeout = Math.min(timeout*1.2 + 5, 3000);
+                            setTimeout(function () {
+                                ws = getWs(nextTimeout);
+                            }, timeout*1000);
+                        };
+                    };
+                });
+            };
+            return innerWs;
+        };
+        var ws = getWs();
+    });
 
 function getCookie(name) {
     var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
@@ -20,11 +70,3 @@ jQuery.postJSON = function(url, args, callback, error) {
     }});
 };
 
-app.days = {'V': 0, 'S':1, 'D':2, 'L':3};
-
-// var padId = $('#wikipad').attr('padId');
-// $('#wikipad').pad({'padId':padId,
-//                    'showChat':'false',
-//                    'host':window.location.protocol + "//" + window.location.hostname+":9001",
-//                    'userName': eventer.name,
-//                    });
--- web/static/js/controllers.js
+++ web/static/js/controllers.js
@@ -1,113 +1,69 @@
-var app = angular.module('club');
-app.callbacks = {};
+var app = angular.module('club').
+    controller('HomeController', ['$scope', function($scope) {
+    }]).
+    controller('MembersController', ['$scope', 'callbacks', function ($scope, callbacks) {
+        $scope.members = eventer.init.members;
 
-app.controller('home', [function () {
-    var getWs = function(timeout) {
-        var innerWs = new WebSocket("ws://"+window.location.hostname+eventer.home+"/updates");
-        innerWs.onmessage = function(evt) {
-	    var data = $.parseJSON(evt.data);
-	    for(var dataKey in data) {
-                var callback = app.callbacks[dataKey]
-                var obj = data[dataKey];
-                if (callback) {
-                    callback(obj);
-                };
-	    };
-        };
-        innerWs.onclose = function (evt) {
-            var nextTimeout = Math.min(timeout*1.2 + 5, 3000);
-            setTimeout(function () {
-                ws = getWs(nextTimeout);
-            }, timeout*1000);
-        }
-        /**
-           Set connection retry timeout to a low value upon a successful connection.
-        */
-        innerWs.onopen = function (evt) {
-            setTimeout(function () {
-                if (innerWs.readyState == WebSocket.OPEN) {
-                    timeout = 1;
-                    innerWs.onclose = function (evt) {
-                        var nextTimeout = Math.min(timeout*1.2 + 5, 3000);
-                        setTimeout(function () {
-                            ws = getWs(nextTimeout);
-                        }, timeout*1000);
+        callbacks.UpdateUser = function(obj) {
+            $scope.$apply(function() {
+                for (var key in $scope.members) {
+                    var member = $scope.members[key];
+                    if (member.name == obj.name) {
+                        $scope.members[key] = obj;
+                        break;
                     };
                 };
             });
         };
-        return innerWs;
-    };
-    var ws = getWs();
-}]);
-
-function MembersController($scope) {
-    $scope.members = eventer.init.members;
-
-    app.callbacks.UpdateUser = function(obj) {
-        $scope.$apply(function() {
-            for (var key in $scope.members) {
-                var member = $scope.members[key];
-                if (member.name == obj.name) {
-                    $scope.members[key] = obj;
-                    break;
-                };
-            };
-        });
-    };
-};
-
-function PresenceController($scope) {
-    $scope.presence = eventer.init.presence;
-    $scope.changed = false;
-
-    $scope.select = function($presence) {
-        var value = ($presence[1]+1)%3;
-        var day = $presence[0];
-        var index = app.days[day];
-        $scope.presence[index][1] = value;
-        $scope.changed = true;
-    }
-
-    $scope.confirm = function() {
+    }]).
+    controller('PresenceController', ['$scope', 'days', function ($scope, days) {
+        $scope.presence = eventer.init.presence;
         $scope.changed = false;
-        var data = {
-            callback: "presence",
-            presence: $scope.presence.map(function(m) {
-                if (typeof m[1] == "number") {
-                        return m[1];
-                }
-                else {
-                    return -1;
-                }
-            })
-        };
-        jQuery.postJSON(eventer.home+"/update", data);
-    }
-};
 
-
-function ChatController($scope) {
-    $scope.messages = [];
-    $scope.chatVisible = false;
-    $scope.toggle = function () {
-        $scope.chatVisible ^= true;
-    };
-    $scope.sendChat = function() {
-        // Don't send empty message
-        if ($scope.chatMessage) {
-            jQuery.postJSON(eventer.home+"/chat", {'message':$scope.chatMessage});
+        $scope.select = function($presence) {
+            var value = ($presence[1]+1)%3;
+            var day = $presence[0];
+            var index = days[day];
+            $scope.presence[index][1] = value;
+            $scope.changed = true;
         }
-        $scope.chatMessage = "";
-    };
-    app.callbacks.ChatMessage = function(message) {
-        $scope.$apply(function() {
-            $scope.messages.push(message);
-            var el = $("#chat .chatMessages");
-            el.animate({scrollTop: el.height()}, 'slow');;
-        });
-    };
-};
 
-function MessageController($scope) {
-}
+        $scope.confirm = function() {
+            $scope.changed = false;
+            var data = {
+                callback: "presence",
+                presence: $scope.presence.map(function(m) {
+                    if (typeof m[1] == "number") {
+                        return m[1];
+                    }
+                    else {
+                        return -1;
+                    }
+                })
+            };
+            jQuery.postJSON(eventer.home+"/update", data);
+        }
+    }]).
+    controller('ChatController', ['$scope', 'callbacks', function ($scope, callbacks) {
+        $scope.messages = [];
+        $scope.chatVisible = false;
+        $scope.toggle = function () {
+            $scope.chatVisible ^= true;
+        };
+        $scope.sendChat = function() {
+            // Don't send empty message
+            if ($scope.chatMessage) {
+                jQuery.postJSON(eventer.home+"/chat", {'message':$scope.chatMessage});
+            }
+            $scope.chatMessage = "";
+        };
+        callbacks.ChatMessage = function(message) {
+            $scope.$apply(function() {
+                $scope.messages.push(message);
+                var el = $("#chat .chatMessages");
+                el.animate({scrollTop: el.height()}, 'slow');;
+            });
+        };
+    }]).
+    controller('MessageController', ['$scope', 'callbacks', function($scope, callbacks) {
+    }]);
